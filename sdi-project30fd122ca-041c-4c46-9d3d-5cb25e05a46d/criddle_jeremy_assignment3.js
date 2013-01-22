fb36df6ba0d9802fa7fd3f389e89e3d273b15089
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-22 01:27:10 -0500

    js update

--- sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
+++ sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
@@ -5,6 +5,19 @@
 
 
 //global variables
+var jj = {name: "JJ",
+    age : 9,
+    color : {tail :"black",
+        feet : "white",
+        body : ["black", "grey"],
+        belly: "white",
+        head: ["black", "white", "grey"]
+    };
+    attitude : "gentle",
+    eatFood: function (){
+
+}
+};
 var famLCats = ["JJ", "Ginger", "Baby"];
 var famJCats = ["Max", "Erma"];
 var famNCats = ["Little Kitty", "Tigger"];
