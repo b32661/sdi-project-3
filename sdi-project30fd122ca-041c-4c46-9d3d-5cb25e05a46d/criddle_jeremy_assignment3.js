22466ee6feefd84db3530b25dc6764d7228f29f0
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-23 08:05:56 -0500

    working

--- sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
+++ sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
@@ -3,9 +3,32 @@
 // Project 3
 // "cats"
 
+var cat = function (name, age, size, family){
+var getName = function(){return name; };
+    return {
+      "name"  : getName,
+      "age"   : age,
+      "size"  : size,
+      "family": family
 
-//my global variables
+    };
 
-//my functions
+};
 
-//my main code
+
+var jj = cat("JJ", "6", "medium", "LFam");
+var ginger = cat("Ginger", "6", "large", "LFam");
+var annastasia = cat("Annastasia", "10", "medium", "LFam");
+var baby = cat("Baby", "2", "medium", "LFam");
+var midnight = cat("Midnight", "5", "medium", "NFam");
+var littleKitty = cat("Little Kitty", "2", "small", "NFam");
+var boots = cat("Boots", "11", "medium", "FFam");
+var fluffy = cat("Fluffy", "12", "medium", "FFam");
+var tilly = cat("Tilly", "1", "medium", "JFam");
+var milly = cat("Milly", "1", "medium", "JFam");
+var cats = [jj, annastasia, baby, boots, fluffy, ginger, littleKitty, midnight, milly, tilly];
+for var i=0; i<cats.length; i++){
+    console.log(cats[i]);
+
+
+}
