41ca34d5667228d09c222a2deb515157f84138d8
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-24 16:52:11 -0500

    cats meow

--- sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
+++ sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
@@ -5,37 +5,46 @@
 
 
 //JSON data
-var json;
-json = {"expenses":[
-       {"catfood":30,
+var json = {"expenses":[
+       {"food":30,
         "litter":20,
         "treats":2}
 ]};
 
 //global meow-iables
 var cats = {cat:[
-    {name:"JJ", age:6, size:"medium", family:"Lara's family"},
-    {name:"Ginger", age:6, size:"large", family:"Lara's family"},
-    {name:"Annastasia", age:10, size:"medium", family:"Lara's family"},
-    {name:"Baby", age:2, size:"medium", family:"Lara's family"},
-    {name:"Midnight", age:5, size:"medium", family:"Nancy's family"},
-    {name:"Little Kitty", age:2, size:"small", family:"Nancy's family"},
-    {name:"Boots", age:11, size:"medium", family:"Kelly's family"},
-    {name:"Fluffy", age:12, size:"medium", family:"Kelly's family"},
-    {name:"Tilly", age:1, size:"medium", family:"Julie's family"},
-    {name:"Milly", age:1, size:"medium", family:"Julie's family"}
+    {name:"JJ", age:6, isLarge:false, family:"Lara's family", favorite : {toy: "ball", food: "fish"}},
+    {name:"Ginger", age:6, isLarge:true, family:"Lara's family", favorite : {toy: "ball of yarn", food: "poultry"}},
+    {name:"Annastasia", age:10, isLarge:false, family:"Lara's family", favorite : {toy: "lazer", food: "beef"}},
+    {name:"Baby", age:4, isLarge:false, family:"Lara's family", favorite : {toy: "ball", food: "poultry"}},
+    {name:"Midnight", age:5, isLarge:false, family:"Nancy's family", favorite : {toy: "lazer", food: "beef"}},
+    {name:"Little Kitty", age:3, isLarge:false, family:"Nancy's family", favorite : {toy: "ball", food: "fish"}},
+    {name:"Boots", age:11, isLarge:false, family:"Kelly's family", favorite : {toy: "ball of yarn", food: "poultry"}},
+    {name:"Fluffy", age:12, isLarge:true, family:"Kelly's family", favorite : {toy: "lazer", food: "fish"}},
+    {name:"Tilly", age:2, isLarge:false, family:"Julie's family", favorite : {toy: "ball", food: "beef"}},
+    {name:"Milly", age:2, isLarge:false, family:"Julie's family", favorite : {toy: "fish", food: "fish"}}
 ]};
 
 //
 
 
 
-//main code
+// main code
+// console log parts of cat object
 for (var currentCat=0; currentCat<cats.cat.length; currentCat++){
     console.log("Hi! My name is " + cats.cat[currentCat].name + " . I am " +
-        cats.cat[currentCat].age + " years old. I am a " +
-        cats.cat[currentCat].size + " sized kitty and I live with the " +
-        cats.cat[currentCat].family + " .");
-    }
-
-
+        cats.cat[currentCat].age + " years old. It is " +
+        cats.cat[currentCat].isLarge + " that I am a large sized kitty and I live with " +
+        cats.cat[currentCat].family + " . I really like to play with a " +
+        cats.cat[currentCat].favorite.toy + ". " + cats.cat[currentCat].favorite.food + " is numee!")
+}
+// this is the cat's meow
+for (var currentCat=0; currentCat<cats.cat.length; currentCat++){
+    cats.cat[currentCat].toPurr =
+        [function toPurr(){
+         console.log("purr");},
+         function toMeow(){
+         console.log("meow");
+    }]
+}
+console.log(cats.cat[4]);
