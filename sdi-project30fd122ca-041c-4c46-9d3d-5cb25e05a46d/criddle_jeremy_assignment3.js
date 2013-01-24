64c7cd39027268ba3df563f7b084c34e6b077332
Jeremy Criddle <jeremycriddle@fullsail.edu>
2013-01-24 13:20:10 -0500

    json data

--- sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
+++ sdi-project30fd122ca-041c-4c46-9d3d-5cb25e05a46d/criddle_jeremy_assignment3.js
@@ -3,33 +3,39 @@
 // Project 3
 // "cats"
 
+
+//JSON data
+var json;
+json = {"expenses":[
+       {"catfood":30,
+        "litter":20,
+        "treats":2}
+]};
+
 //global meow-iables
 var cats = {cat:[
-    {name:"JJ", age:6, size:"medium", family:"LF"},
-    {name:"Ginger", age:6, size:"large", family:"LF"},
-    {name:"Annastasia", age:10, size:"medium", family:"LF"},
-    {name:"Baby", age:2, size:"medium", family:"LF"},
-    {name:"Midnight", age:5, size:"medium", family:"NF"},
-    {name:"Little Kitty", age:2, size:"small", family:"NF"},
-    {name:"Boots", age:11, size:"medium", family:"FF"},
-    {name:"Fluffy", age:12, size:"medium", family:"FF"},
-    {name:"Tilly", age:1, size:"medium", family:"JF"},
-    {name:"Milly", age:1, size:"medium", family:"JF"}
+    {name:"JJ", age:6, size:"medium", family:"Lara's family"},
+    {name:"Ginger", age:6, size:"large", family:"Lara's family"},
+    {name:"Annastasia", age:10, size:"medium", family:"Lara's family"},
+    {name:"Baby", age:2, size:"medium", family:"Lara's family"},
+    {name:"Midnight", age:5, size:"medium", family:"Nancy's family"},
+    {name:"Little Kitty", age:2, size:"small", family:"Nancy's family"},
+    {name:"Boots", age:11, size:"medium", family:"Kelly's family"},
+    {name:"Fluffy", age:12, size:"medium", family:"Kelly's family"},
+    {name:"Tilly", age:1, size:"medium", family:"Julie's family"},
+    {name:"Milly", age:1, size:"medium", family:"Julie's family"}
 ]};
 
 //
-var clarifyFamily = {LF: function(){LF = "Lara's family"},
-    NF: function (){NF = "Nancy's family"},
-    FF: function (){FF = "Kelly's family"},
-    JF: function(){JF = "Julie's family"}};
 
 
 
-//main comfy cozy code
+//main code
 for (var currentCat=0; currentCat<cats.cat.length; currentCat++){
     console.log("Hi! My name is " + cats.cat[currentCat].name + " . I am " +
         cats.cat[currentCat].age + " years old. I am a " +
         cats.cat[currentCat].size + " sized kitty and I live with the " +
-        cats.cat[currentCat].family + " family. My family name might be a little unclear, we need to clarifyFamily()");
-}
+        cats.cat[currentCat].family + " .");
+    }
+
 
